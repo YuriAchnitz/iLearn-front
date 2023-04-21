6ab66d2c293b372ec4285fac24939f343f2df059
Yuri Achnitz <yuri_achnitz@hotmail.com>
2023-04-21 18:26:57 -0300

    Made basic screen framework

---
+++ components/screens/SignUpScreen.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function SignUpScreen() {
+  return (
+    <View>
+      <Text>This is the sign up screen</Text>
+    </View>
+  );
+}
